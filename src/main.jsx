eead521d9350853363ddd6532dc7d0004f4944d2
Christina Rontell <clrontell@gmail.com>
2023-01-19 16:30:33 -0600

    react router error-page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ErrorPage from './error-page'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App/>,
+    element: <App />,
+    errorElement: <ErrorPage/>,
   },
 ]);
 
