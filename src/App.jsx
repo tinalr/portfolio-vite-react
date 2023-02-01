aaa9fb1523b6485093ec309dd6aca2ac11460213
Christina Rontell <clrontell@gmail.com>
2023-01-31 23:59:33 -0600

    set up About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Outlet, Link } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Socials from './components/Socials';
+import About from './components/About';
 import './App.css'
 
 function App() {
@@ -9,7 +9,8 @@ function App() {
     <>
       <h1 className='text-3xl font-bold underline'>Home Page</h1>
       <Navigation />
-      <Socials/>
+      <Socials />
+      <About/>
     </>
     
   );
