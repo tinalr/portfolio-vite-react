cb52a59b10010fa847854e339819fb12fc8db4de
Christina Rontell <clrontell@gmail.com>
2023-02-01 00:08:27 -0600

    filler

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,13 @@
 
 const About = () => {
   return (
-    <></>
+    <>
+      <main>
+        <h1>Hi, I'm Christina</h1>
+        <h2>Frontend Developer</h2>
+        <p>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.<br /> The quick brown fox jumped over the lazy dog.</p>
+      </main>
+    </>
   )
 }
 export default About
