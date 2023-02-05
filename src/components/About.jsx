9c030c44092c52763bae14f3ab60dbd2eab7d606
Christina Rontell <clrontell@gmail.com>
2023-02-04 23:59:42 -0600

    styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@
 const About = () => {
   return (
     <>
-      <main>
+      <main style={style}>
         <h1>Hi, I'm Christina</h1>
         <h2>Frontend Developer</h2>
         <p>The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.<br /> The quick brown fox jumped over the lazy dog.</p>
